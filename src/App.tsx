4192e24aca738504e3019857485b506500121771
yatochkaaa <jaffplay@gmail.com>
2023-03-09 16:14:15 +0200

    update package.json, .eslintrc and lint project

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
 
 const App: React.FC = () => {
   return (
     <div></div>
-  );
+  )
 }
 
-export default App;
+export default App
